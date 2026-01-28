4ba3edd78cdb7ea6019a37192ad562dee9959987
yarloe <114802802+yarloe@users.noreply.github.com>
2026-01-28 17:36:06 +0800

    Add files via upload

---
+++ kubejs/server_scripts/lucky_attack.js
@@ -0,0 +1,31 @@
+
+EntityEvents.hurt(event => { 
+    let { source, entity } = event;
+    let player = source.player;
+    if (player && source.getType() == "player") {
+        if (entity.getTags().contains("attacked")) {
+            Utils.server.scheduleInTicks(8, () => {
+                if (entity.isAlive()) {
+                    entity.removeTag("attacked");
+                    return;
+                }
+            });
+        }
+        else{    
+            let l = player.getAttributeValue("generic.luck");
+            if(l > 100 )
+                l = 100;
+            let lr = l / (l+100);
+            let lt = 0.4 + l/400;
+            entity.addTag("attacked");
+            if(Math.random() < lr && entity.isAlive()){
+                Utils.server.scheduleInTicks(1, () => {
+                    let damage = player.getAttributeValue("generic.attack_damage");
+                    entity.invulnerableTime = 0;
+                    entity.attack(source, damage*lt*(1+lr));
+                    event.level.spawnParticles('minecraft:dust 1 0 0 1', true, entity.x, entity.y + 1, entity.z, 0.2, 0.5, 0.2, 10, 0.05);
+                })
+            };
+        }
+    }
+});
