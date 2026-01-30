fda58f36435e230fc30bc31a5f2def3af87e86f6
yarloe <2723774905@qq.com>
2026-01-30 14:41:51 +0800

    Update lucky_attack.js

--- kubejs/server_scripts/lucky_attack.js
+++ kubejs/server_scripts/lucky_attack.js
@@ -15,17 +15,20 @@ EntityEvents.hurt(event => {
             let l = player.getAttributeValue("generic.luck");
             if(l > 100 )
                 l = 100;
-            let lr = l / (l+100);
-            let lt = 0.4 + l/400;
+            let lr = 0.05 + ( l / ( l + 100 ));
+            let lt = 0.4 + l / 400;
             entity.addTag("attacked");
             if(Math.random() < lr && entity.isAlive()){
-                Utils.server.scheduleInTicks(1, () => {
+                Utils.server.scheduleInTicks(0, () => {
                     let damage = player.getAttributeValue("generic.attack_damage");
                     entity.invulnerableTime = 0;
                     entity.attack(source, damage*lt*(1+lr));
                     event.level.spawnParticles('minecraft:dust 1 0 0 1', true, entity.x, entity.y + 1, entity.z, 0.2, 0.5, 0.2, 10, 0.05);
                 })
             };
+            Utils.server.scheduleInTicks(1, () => {
+                entity.removeTag("attacked");
+            })
         }
     }
 });
